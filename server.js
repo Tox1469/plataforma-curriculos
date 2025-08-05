e5644866449bd538fd76c02c0358a052a50674b3
Tox1469 <Toxjooj@gmail.com>
2025-08-05 02:58:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+// Servir arquivos estáticos da pasta public
+app.use(express.static('public'));
+
+// Rota principal redireciona para index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 require('./models/init'); // cria tabelas se não existirem
 
 app.use('/auth', require('./routes/auth'));
